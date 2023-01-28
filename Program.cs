25451ab3239d1d8c5274d2d5b1625268908ae3d2
gharjare <gharjarechetan@gmail.com>
2023-01-28 15:43:20 +0530

    [chetan gharjare]Here reverse number program complete

--- Program.cs
+++ Program.cs
@@ -5,22 +5,19 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programs");
+            int num, rem, reverse = 0;
+
             Console.WriteLine("Enter the number");
-            int num = Convert.ToInt32(Console.ReadLine());
-            bool IsPrime = true;
-            for (int i = 2; i < num / 2; i++)
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 0)
             {
-                if (num % i == 0)
-                {
-                    IsPrime = false;
-                    Console.WriteLine("Is not prime");
-                    break;
-                }
+                rem = num % 10;
+                reverse = reverse * 10 + rem;
+                num = num / 10;
             }
-            if (IsPrime)
-            {
-                Console.WriteLine("Prime Number");
-            }
+            Console.WriteLine("Reverse number " + reverse);
         }
+    }
     }
 }
