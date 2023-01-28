cd972aceca23618b2c9da7d58d979b367c3b3190
gharjare <gharjarechetan@gmail.com>
2023-01-28 15:15:24 +0530

    [chetan gharjare]Here fibonacci series prgram complete

--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programs");
-            
+            int a = 0, b = 1, c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                Console.WriteLine(c);
+            }
         }
     }
 }
